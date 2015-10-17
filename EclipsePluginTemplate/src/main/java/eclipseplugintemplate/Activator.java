cff185c16c2ff5718baa086189af72a55ad93395
oleng <oleng_rama@hotmail.com>
2015-10-17 10:00:47 -0400

    testing

--- EclipsePluginTemplate/src/main/java/eclipseplugintemplate/Activator.java
+++ EclipsePluginTemplate/src/main/java/eclipseplugintemplate/Activator.java
@@ -11,6 +11,7 @@ public class Activator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "EclipsePluginTemplate"; //$NON-NLS-1$
 
+	
 	// The shared instance
 	private static Activator plugin;
 	
